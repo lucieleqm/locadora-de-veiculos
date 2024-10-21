6c20130fc0694dfd851646f186e3aaf109870ac9
Luciele Miranda <114947493+LucieleQM@users.noreply.github.com>
2024-10-20 21:17:16 -0300

    Criei o layout principal e Carreguei as fontes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,24 +1,34 @@
-/*import {Slot} from "expo-router";
-
-
-
-export default function Layout() {
-    return(
-        <Slot/>
-    )
-}*/
+import { Slot } from "expo-router";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from "expo-splash-screen"
 
 import "../styles/global.css";
-//import { Foundation} from "@expo/vector-icons";
 
-    /*<Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>*/
- // );
-//}
-/*
-import DrawerLayout from './(drawer)/_layout';
+import {
+  useFonts,
+  Roboto_300Light,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+ 
+SplashScreen.preventAutoHideAsync()
 
-export default function AppLayout() {
-  return <DrawerLayout />;
-}*/
+export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Roboto_300Light,
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded){
+    SplashScreen.hideAsync()
+  }
+
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Slot />
+    </GestureHandlerRootView>
+  );
+}
