2bb0b35976c7d6a3735d3ce3a5dbea74109765ee
Luciele Miranda <114947493+LucieleQM@users.noreply.github.com>
2024-11-02 18:42:39 -0300

    update tela locacoes

--- src/app/(drawer)/(tabs)/locacoes/index.tsx
+++ src/app/(drawer)/(tabs)/locacoes/index.tsx
@@ -3,11 +3,14 @@ import { SafeAreaView} from 'react-native'
 import AddButton from '../../../../components/Button/AddButton'
 import { router } from 'expo-router'
 
+import ListLocacao from '../../../../components/List/ListLocacao'
+
 import styles from '../../../../styles/style'
 
 export default function Locacoes(){
     return (
       <SafeAreaView style={styles.container}>
+        <ListLocacao/>
         <AddButton onPress={()=>router.push('../../../addLocacao')}/>
       </SafeAreaView>
     )
