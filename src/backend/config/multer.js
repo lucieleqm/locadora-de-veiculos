895860557ce8b252a5a2f8286ac67e1f72b706a9
Luciele Miranda <114947493+LucieleQM@users.noreply.github.com>
2024-11-02 18:37:57 -0300

    config do multer

---
+++ src/backend/config/multer.js
@@ -0,0 +1,16 @@
+// Configurações do multer para upload de arquivos 
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
